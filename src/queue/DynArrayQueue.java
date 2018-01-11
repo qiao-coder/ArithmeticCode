b833d748e7418b257650b2f29aa88631db762056
tufei <1024522245@qq.com>
2018-01-11 10:30:59 +0800

    队列：基于动态循环数组实现队列

--- src/queue/DynArrayQueue.java
+++ src/queue/DynArrayQueue.java
@@ -3,44 +3,45 @@ package queue;
 import java.lang.reflect.Array;
 
 /**
- * 队列：简单循环数组的实现
+ * 队列：动态循环数组的实现
  *
  * @author tufei
  * @date 2018/1/10.
  */
 
-public class ArrayQueue<T> {
+public class DynArrayQueue<T> {
 
-    private final int capacity;
-    private final T[] array;
+    private final Class<T> kind;
+    private int capacity;
+    private T[] array;
     private int front = -1;
     private int end = -1;
 
     @SuppressWarnings("unchecked")
-    private ArrayQueue(Class<T> kind, int size) {
+    private DynArrayQueue(Class<T> kind, int size) {
         this.capacity = size;
+        this.kind = kind;
         array = (T[]) Array.newInstance(kind, size);
     }
 
-    public static <T> ArrayQueue<T> create(Class<T> kind, int size) {
+    public static <T> DynArrayQueue<T> create(Class<T> kind, int size) {
         if(kind == null){
             throw new IllegalArgumentException("kind can't be null!");
         }
         if(size == 0){
             throw new IllegalArgumentException("size can't be zero!");
         }
-        return new ArrayQueue<>(kind, size);
+        return new DynArrayQueue<>(kind, size);
     }
 
     public void enQueue(T data) {
         if (isFull()) {
-            throw new QueueOverflowException();
-        } else {
-            end = (end + 1) % capacity;
-            array[end] = data;
-            if (front == -1) {
-                front = end;
-            }
+            resizeQueue();
+        }
+        end = (end + 1) % capacity;
+        array[end] = data;
+        if (front == -1) {
+            front = end;
         }
     }
 
@@ -76,30 +77,43 @@ public class ArrayQueue<T> {
     }
 
     public int size() {
+        //两种情况：一种end在front前面  一种end在front后面（所以加capacity）
         if (isEmpty()) {
             return 0;
         }
         if (isFull()) {
             return capacity;
         }
-        //两种情况：一种end在front前面  一种end在front后面（所以加capacity）
         return (end - front + 1 + capacity) % capacity;
     }
 
+    @SuppressWarnings("unchecked")
+    private void resizeQueue() {
+        int newCapacity = capacity * 2;
+        T[] newArray = (T[]) Array.newInstance(kind, newCapacity);
+        int cursor = front;
+        for (; cursor < capacity; cursor++) {
+            newArray[cursor] = array[cursor];
+        }
+        if (front > end) {
+            for (int newCursor = 0; newCursor <= end; cursor++) {
+                newArray[cursor] = array[newCursor];
+            }
+        }
+        capacity = newCapacity;
+        //必须减1，因为游标最后会移动多一位
+        end = cursor - 1;
+        array = newArray;
+    }
+
     public static void main(String[] args) {
-        ArrayQueue<Integer> integerArrayQueue = ArrayQueue.create(Integer.class, 6);
-        integerArrayQueue.enQueue(1);
-        System.out.println("入队：1,队首元素现在是：" + integerArrayQueue.front() + "，队列长度是：" + integerArrayQueue.size());
-        integerArrayQueue.enQueue(2);
-        System.out.println("入队：2,队首元素现在是：" + integerArrayQueue.front() + "，队列长度是：" + integerArrayQueue.size());
-        System.out.println("出队：" + integerArrayQueue.deQueue() + ",队首元素现在是：" + integerArrayQueue.front() + "，队列长度是：" + integerArrayQueue.size());
-        System.out.println("出队：" + integerArrayQueue.deQueue() + ",队首元素现在是：" + integerArrayQueue.front() + "，队列长度是：" + integerArrayQueue.size());
-        System.out.println("---------------------------------------------");
-        ArrayQueue<String> stringArrayQueue = ArrayQueue.create(String.class, 2);
+        DynArrayQueue<String> stringArrayQueue = DynArrayQueue.create(String.class,1);
         stringArrayQueue.enQueue("a");
         System.out.println("入队：a,队首元素现在是：" + stringArrayQueue.front() + "，队列长度是：" + stringArrayQueue.size());
         stringArrayQueue.enQueue("b");
         System.out.println("入队：b,队首元素现在是：" + stringArrayQueue.front() + "，队列长度是：" + stringArrayQueue.size());
+        stringArrayQueue.enQueue("c");
+        System.out.println("入队：c,队首元素现在是：" + stringArrayQueue.front() + "，队列长度是：" + stringArrayQueue.size());
         System.out.println("出队：" + stringArrayQueue.deQueue() + ",队首元素现在是：" + stringArrayQueue.front() + "，队列长度是：" + stringArrayQueue.size());
         System.out.println("出队：" + stringArrayQueue.deQueue() + ",队首元素现在是：" + stringArrayQueue.front() + "，队列长度是：" + stringArrayQueue.size());
     }
