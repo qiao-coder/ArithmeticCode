d5670f6b56864630ed40c3cde4a6f1b6a935c5c5
tufei <1024522245@qq.com>
2018-01-31 11:27:44 +0800

    树：二叉搜索树查找元素，查找最小值

--- src/tree/BinarySearchTreeNode.java
+++ src/tree/BinarySearchTreeNode.java
@@ -1,12 +1,10 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import queue.Queue;
+import static util.Print.print;
 
 /**
  * 二叉搜索树：和一般二叉树没区别，区别在于数据，而不是结构
+ *
  * @author tufei
  * @date 2018/1/22.
  */
@@ -41,42 +39,62 @@ public class BinarySearchTreeNode<T> {
         this.right = right;
     }
 
-    public int size() {
-        size = 0;
-        return preOrder(this);
-    }
-
-    private int preOrder(BinarySearchTreeNode<T> treeNode) {
-        if (treeNode != null) {
-            size++;
-            preOrder(treeNode.getLeft());
-            preOrder(treeNode.getRight());
+    /**
+     * 寻找元素
+     *
+     * @param root
+     * @param data
+     * @return
+     */
+    public static BinarySearchTreeNode<Integer> find(BinarySearchTreeNode<Integer> root, int data) {
+        if (root == null) {
+            return null;
         }
-        return size;
+        if (root.getData() > data) {
+            return find(root.getLeft(), data);
+        }
+        if (root.getData() < data) {
+            return find(root.getRight(), data);
+        }
+        return root;
     }
 
-    private void levelOrder(BinarySearchTreeNode<T> treeNode, List<T> list) {
-        if (treeNode != null) {
-            Queue<BinarySearchTreeNode<T>> queue = new Queue<>();
-            queue.enQueue(treeNode);
-            BinarySearchTreeNode<T> temp;
-            while (!queue.isEmpty()) {
-                temp = queue.deQueue();
-                list.add(temp.getData());
-                if (temp.getLeft() != null) {
-                    queue.enQueue(temp.getLeft());
-                }
-                if (temp.getRight() != null) {
-                    queue.enQueue(temp.getRight());
-                }
-            }
+    public static BinarySearchTreeNode<Integer> findMin(BinarySearchTreeNode<Integer> root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.getLeft() == null) {
+            return root;
+        } else {
+            return findMin(root.getLeft());
         }
     }
 
-    @Override
-    public String toString() {
-        List<T> list = new ArrayList<>();
-        levelOrder(this, list);
-        return list.toString();
+    public static void main(String[] args) {
+        BinarySearchTreeNode<Integer> treeNode = new BinarySearchTreeNode<>();
+        treeNode.setData(5);
+        BinarySearchTreeNode<Integer> treeNode1 = new BinarySearchTreeNode<>();
+        treeNode1.setData(4);
+        BinarySearchTreeNode<Integer> treeNode2 = new BinarySearchTreeNode<>();
+        treeNode2.setData(6);
+        BinarySearchTreeNode<Integer> treeNode3 = new BinarySearchTreeNode<>();
+        treeNode3.setData(2);
+        BinarySearchTreeNode<Integer> treeNode4 = new BinarySearchTreeNode<>();
+        treeNode4.setData(3);
+        BinarySearchTreeNode<Integer> treeNode5 = new BinarySearchTreeNode<>();
+        treeNode5.setData(7);
+        treeNode.setLeft(treeNode1);
+        treeNode.setRight(treeNode2);
+        treeNode1.setLeft(treeNode3);
+        treeNode1.setRight(treeNode4);
+        treeNode2.setRight(treeNode5);
+
+
+        print(treeNode3.equals(find(treeNode, 2)));
+        print(treeNode4.equals(find(treeNode, 7)));
+        print(treeNode5.equals(find(treeNode, 7)));
+        print("---------------------------------");
+        print(treeNode2.equals(findMin(treeNode)));
+        print(treeNode3.equals(findMin(treeNode)));
     }
 }
